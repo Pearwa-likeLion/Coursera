70ed946e90b7cd5f07ac17c976d387fd5f5e76b3
Pearwa-likeLion <FITTOKUNG@GMAIL.COM>
2021-03-27 10:39:45 +0700

    Practice coding

---
+++ Coding_Highschool(ม.4)/switch/switch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace switchreal
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Input your grade : ");
+        char i = Convert.ToChar( Console.ReadLine());
+        switch (i)
+        {
+            case 'A' :
+                Console.WriteLine("Well Done!!!");
+                break;
+            default :
+                Console.WriteLine("Error");
+                break;
+
+        }
+    }
+}
+}
---
+++ Coding_Highschool(ม.4)/testarraystruct/testarraystruct/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+    struct schoolDataArray
+        {
+    public string namestd;
+    public int passWordStd;
+    public int pinStd;
+        }
+
+namespace testarraystruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            schoolDataArray[] dataArrayStd = new schoolDataArray[5];
+            Console.WriteLine("Hello teacher!!!!");
+            Console.WriteLine("---------------------------------------");
+            for (i = 0;i<dataArrayStd.Length;i++)
+            {
+                Console.Write("Please enter your students name : ");
+                dataArrayStd[i].namestd = Console.ReadLine();
+            }
+            Console.WriteLine("----------------------------------");
+            for(int k = 0; k < dataArrayStd.Length; k++)
+            {
+                
+                Console.WriteLine("Name student "+k+" :"+dataArrayStd[k].namestd);
+                Console.WriteLine("-------------------------------");
+            }
+
+        }
+    }
+}
